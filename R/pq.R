985e246d6a11fc3d0cc350874acd381db55ebad8
Grant Williamson <Grant.Williamson@utas.edu.au>
2022-06-24 09:15:12 +1000

    Removed pq documentation until I understand how to do this...

--- R/pq.R
+++ R/pq.R
@@ -1,11 +1,4 @@
-#' Print non-quiet information.
-#'
-#' @param text Text to be printed
-#' @param quiet TRUE/FALSE
-#' @return NULL
-#'
-#' @examples pq("Hello",TRUE)
-#' @NoRd
+
 pq <- function(text,quiet){
   if(!quiet){
     print(text)
