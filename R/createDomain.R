49e46014f8f6c620f94c2374da913a455443421f
Grant Williamson <Grant.Williamson@utas.edu.au>
2022-06-24 09:08:33 +1000

    Changing how we treat internal pq function, calling explicitly internal

--- R/createDomain.R
+++ R/createDomain.R
@@ -19,43 +19,43 @@
 createDomain <- function(domain,res=100,proj,outdir,quiet=TRUE,overwrite=TRUE){
   # Work out if input is vect or sf, convert to appropriate
   if("sf" %in% class(domain)){
-    pq("Found sf object, converting to SpatVector.",quiet)
+    fiRetools:::pq("Found sf object, converting to SpatVector.",quiet)
     domain <- terra::vect(domain)
   }
 
   # Do we have a projection? If not get it from input vect:
   if(!is.null(proj)){
-    pq("Projection provided, projecting input domain.",quiet)
+    fiRetools:::pq("Projection provided, projecting input domain.",quiet)
     domain <- terra::project(domain,proj)
   }else{
-    pq("No projection provided, using projection from input domain.",quiet)
+    fiRetools:::pq("No projection provided, using projection from input domain.",quiet)
   }
 
   proj <- paste0("epsg:",crs(domain,describe=TRUE)$code)
-  pq(paste0("Projection is: ",proj),quiet)
+  fiRetools:::pq(paste0("Projection is: ",proj),quiet)
 
   # aggregate to single polygon
-  pq("Aggregating domain polygons into single polygon.",quiet)
+  fiRetools:::pq("Aggregating domain polygons into single polygon.",quiet)
   domain <- terra::aggregate(domain)
 
   # create raster
-  pq("Creating template raster.",quiet)
+  fiRetools:::pq("Creating template raster.",quiet)
   template <- rast(terra::ext(domain),res=res,crs=proj)
-  pq(paste0("Raster extent: ",terra::ext(template)),quiet)
-  pq(paste0("Raster resolution: ",terra::res(template)[1]," x ",terra::res(template)[2]),quiet)
-  pq(paste0("Raster size: ",terra::ncol(template)," x ",terra::nrow(template)),quiet)
+  fiRetools:::pq(paste0("Raster extent: ",terra::ext(template)),quiet)
+  fiRetools:::pq(paste0("Raster resolution: ",terra::res(template)[1]," x ",terra::res(template)[2]),quiet)
+  fiRetools:::pq(paste0("Raster size: ",terra::ncol(template)," x ",terra::nrow(template)),quiet)
   values(template)=1
 
-  pq("Masking template by domain.",quiet)
+  fiRetools:::pq("Masking template by domain.",quiet)
   mask <- terra::mask(template,domain)
 
 
-  pq("Writing template.",quiet)
+  fiRetools:::pq("Writing template.",quiet)
 
   terra::writeRaster(template,paste0(outdir,"/template.tif"),overwrite=overwrite)
-  pq("Writing mask",quiet)
+  fiRetools:::pq("Writing mask",quiet)
   terra::writeRaster(mask,paste0(outdir,"/mask.tif"),overwrite=overwrite)
-  pq("Writing vector ROI",quiet)
+  fiRetools:::pq("Writing vector ROI",quiet)
   terra::writeVector(domain,paste0(outdir,"/ROI.gpkg"),overwrite=overwrite)
 
   out$template <- terra::rast(paste0(outdir,"/template.tif"))
--- R/pq.R
+++ R/pq.R
@@ -2,10 +2,10 @@
 #'
 #' @param text Text to be printed
 #' @param quiet TRUE/FALSE
-#' @keywords internal
 #' @return NULL
 #'
 #' @examples pq("Hello",TRUE)
+#' @NoRd
 pq <- function(text,quiet){
   if(!quiet){
     print(text)
