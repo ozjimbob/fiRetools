55f97592d86bc4f541811d3cc12f12c111ba79fa
Grant Williamson <grant.williamson@utas.edu.au>
2025-01-28 12:18:14 +1100

    Make slope calc more tolerant of missing values (assumes flat / average)

--- R/sslp.R
+++ R/sslp.R
@@ -9,6 +9,11 @@
 #' @examples
 #' sslp(c(20,30,45,40,80,91))
 sslp <- function(y,dist=150){
+  if(all(is.na(y))){
+    y=rep(0,length(y))
+  }
+  mny = mean(y,na.rm=TRUE)
+  y[is.na(y)]=mny
   x=seq(0,dist,length.out=length(y))
   m = stats::lm(y~x)
   ang = tan(stats::coef(m)[2])/pi*180
